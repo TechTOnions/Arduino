24cea1f7b683903fdb10947b13467d9fa7f93db2
TirthBhavani9109 <tirth@techtonions.com>
2021-01-25 17:10:28 +0530

    Update LED_blink_using_millis.ino

--- 6 Simple Eays To Blink Arduino LED/LED_blink_using_millis/LED_blink_using_millis.ino
+++ 6 Simple Eays To Blink Arduino LED/LED_blink_using_millis/LED_blink_using_millis.ino
@@ -1,3 +1,5 @@
+
+
 /*
   Date: 23-01-21
   Code writen by: Dharmik
@@ -6,15 +8,12 @@
   Code uses 866 bytes of program memory and Global variables use 14 bytes
   Find more on www.TechTOnions.com
 */
-
 #define LED 13                  // Defining an LED variable as 13 because our LED is connected to pin 13
 unsigned long previousMillis;   // variable for comparing millis counter
-boolean ledState = false;       // will determine current led state
-
+bool ledState = false;       // will determine current led state
 void setup() {
   pinMode(LED, OUTPUT);         // Set LED pin as OUTPUT
 }
-
 void loop() {
   if (millis() - previousMillis >= 1000)  // check that 1000ms is pass
   {
