ab69c2a25b6f1dc19f1983708141bce9326807a6
Dharmik Patel <43682902+dharmik768@users.noreply.github.com>
2021-05-01 13:18:20 +0530

    Update LED_blink_with_timer_interrupt.ino

--- 6 Simple Eays To Blink Arduino LED/LED_blink_with_timer_interrupt/LED_blink_with_timer_interrupt.ino
+++ 6 Simple Eays To Blink Arduino LED/LED_blink_with_timer_interrupt/LED_blink_with_timer_interrupt.ino
@@ -1,6 +1,6 @@
 /*
   Date: 23-01-21
-  Code writen by: Dharmik
+  Code written by: Dharmik
   LED blink code using timer interrupt
   Code is non blocking code and tested on Arduino UNO, Nano, Mega
   Code uses 880 bytes of program memory and Global variables use 10 bytes
